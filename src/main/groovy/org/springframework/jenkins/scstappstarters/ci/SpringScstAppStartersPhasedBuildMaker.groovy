fdc4f69307a3e35b75c72c21596daec0e81a183d
Soby Chacko <schacko@pivotal.io>
2020-01-16 16:01:34 -0500

    Adding release train build for Fahrenheit

--- src/main/groovy/org/springframework/jenkins/scstappstarters/ci/SpringScstAppStartersPhasedBuildMaker.groovy
+++ src/main/groovy/org/springframework/jenkins/scstappstarters/ci/SpringScstAppStartersPhasedBuildMaker.groovy
@@ -54,23 +54,14 @@ class SpringScstAppStartersPhasedBuildMaker implements SpringScstAppStarterJobs 
                         counter++;
                 }
 
-//                if (!isRelease) {
-//                    phase('app-starters-release-phase') {
-//                        String prefixedProjectName = prefixJob("app-starters-release")
-//                        if (branchToBuild.equals("1.3.x")) {
-//                            branchToBuild = "Celsius"
-//                        }
-//                        else if (branchToBuild.equals("2.0.x")) {
-//                            branchToBuild = "Darwin"
-//                        }
-//                        else if (branchToBuild.equals("2.1.x")) {
-//                            branchToBuild = "Einstein"
-//                        }
-//                        phaseJob("${prefixedProjectName}-${branchToBuild}-ci".toString()) {
-//                            currentJobParameters()
-//                        }
-//                    }
-//                }
+                if (!isRelease) {
+                    phase('app-starters-release-phase') {
+                        String prefixedProjectName = prefixJob("app-starters-release")
+                        phaseJob("${prefixedProjectName}-${branchToBuild}-ci".toString()) {
+                            currentJobParameters()
+                        }
+                    }
+                }
             }
         }
     }
